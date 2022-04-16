912eb7d5ffe417557f811532053932ecde25011a
galustoff <galustoff.dan@yandex.ru>
2022-04-16 18:47:15 +0300

    add form and input in double cheese

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -1,9 +1,14 @@
 const windowWidth = document.documentElement.clientWidth;
 const promoNextBtn = document.querySelector(".promo__next-btn");
+const orderForm = document.forms.order;
 
 controlPromoBtnText();
 
 window.addEventListener('resize', controlPromoBtnText);
+
+orderForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+})
 
 function controlPromoBtnText () {
   promoNextBtn.textContent = (windowWidth < 576) ? "Оформить заказ" : "Далее";
