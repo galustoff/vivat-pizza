a6a3bae53a84c65443f548e13f9cfaf150086484
galustoff <galustoff.dan@yandex.ru>
2022-04-18 07:02:51 +0300

    fix script: submit btn text

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -1,15 +1,15 @@
-const windowWidth = document.documentElement.clientWidth;
+const dde = document.documentElement;
 const promoNextBtn = document.querySelector(".promo__next-btn");
 const orderForm = document.forms.order;
 
 controlPromoBtnText();
 
-window.addEventListener('resize', controlPromoBtnText);
+window.addEventListener("resize", controlPromoBtnText);
 
 orderForm.addEventListener("submit", (e) => {
   e.preventDefault();
-})
+});
 
-function controlPromoBtnText () {
-  promoNextBtn.textContent = (windowWidth < 576) ? "Оформить заказ" : "Далее";
+function controlPromoBtnText() {
+  promoNextBtn.textContent = dde.clientWidth < 576 ? "Оформить заказ" : "Далее";
 }
